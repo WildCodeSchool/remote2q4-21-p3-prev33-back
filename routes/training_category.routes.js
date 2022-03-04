371a03aea36728472bfcecfae3cad611faac0646
sam-rached <rachedsamir@gmail.com>
2022-03-04 15:40:14 +0100

    update

--- routes/training_category.routes.js
+++ routes/training_category.routes.js
@@ -1,101 +1,107 @@
 const connection = require("../db-config");
 const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    connection.query('SELECT * FROM training_category', (err, result) => {
-      if (err) {
-        res.status(500).send('Error retrieving training category from database');
-      } else {
-        res.json(result);
-      }
-    });
+router.get("/", (req, res) => {
+  connection.query("SELECT * FROM training_category", (err, result) => {
+    if (err) {
+      res.status(500).send("Error retrieving training category from database");
+    } else {
+      res.json(result);
+    }
   });
+});
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const trainingCategoryId = req.params.id;
   connection.query(
-    'SELECT * FROM training_category WHERE id = ?',
+    "SELECT * FROM training_category WHERE id = ?",
     [trainingCategoryId],
     (err, results) => {
       if (err) {
-        res.status(500).send('Error retrieving training category from database');
+        res
+          .status(500)
+          .send("Error retrieving training category from database");
       } else {
         if (results.length) res.json(results[0]);
-        else res.status(404).send('training category not found');
+        else res.status(404).send("training category not found");
       }
     }
   );
 });
 
-router.get('/:id/trainings', (req, res) => {
+router.get("/:id/trainings", (req, res) => {
   const trainingCategoryId = req.params.id;
   connection.query(
-    'SELECT title, name, image FROM training \
-     INNER JOIN training_category ON training.training_category_id = training_category.id WHERE training_category.id =?;',
+    "SELECT title, name, image FROM training \
+     INNER JOIN training_category ON training.training_category_id = training_category.id WHERE training_category.id =?;",
     [trainingCategoryId],
     (err, results) => {
       if (err) {
-        res.status(500).send('Error retrieving training from database');
+        res.status(500).send("Error retrieving training from database");
       } else {
         if (results.length) res.json(results);
-        else res.status(404).send('training not found');
+        else res.status(404).send("training not found");
       }
     }
   );
 });
 
-
-
-router.post('/', (req, res) => {
-  const { name, image,} = req.body;
+router.post("/", (req, res) => {
+  const { name, image } = req.body;
   connection.query(
-    'INSERT INTO training_category (name, image) VALUES (?, ?)',
+    "INSERT INTO training_category (name, image) VALUES (?, ?)",
     [name, image],
     (err, result) => {
       if (err) {
         console.error(err);
-        res.status(500).send('Error saving the training category');
+        res.status(500).send("Error saving the training category");
       } else {
         const id = result.insertId;
-        const createdtrainingCategory = { id, name, image};
+        const createdtrainingCategory = { id, name, image };
         res.status(201).json(createdtrainingCategory);
       }
     }
   );
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   const trainingCategoryId = req.params.id;
   const db = connection.promise();
   let existingtrainingCategory = null;
-  db.query('SELECT * FROM training_category WHERE id = ?', [trainingCategoryId])
+  db.query("SELECT * FROM training_category WHERE id = ?", [trainingCategoryId])
     .then(([results]) => {
       existingtrainingCategory = results[0];
-      if (!existingtrainingCategory) return Promise.reject('RECORD_NOT_FOUND');
-      return db.query('UPDATE training_category SET ? WHERE id = ?', [req.body, trainingCategoryId]);
+      if (!existingtrainingCategory) return Promise.reject("RECORD_NOT_FOUND");
+      return db.query("UPDATE training_category SET ? WHERE id = ?", [
+        req.body,
+        trainingCategoryId,
+      ]);
     })
     .then(() => {
       res.status(200).json({ ...existingtrainingCategory, ...req.body });
     })
     .catch((err) => {
       console.error(err);
-      if (err === 'RECORD_NOT_FOUND')
-        res.status(404).send(`trainingCategory with id ${trainingCategoryId} not found.`);
-      else res.status(500).send('Error updating a training category');
+      if (err === "RECORD_NOT_FOUND")
+        res
+          .status(404)
+          .send(`trainingCategory with id ${trainingCategoryId} not found.`);
+      else res.status(500).send("Error updating a training category");
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   connection.query(
-    'DELETE FROM training_category WHERE id = ?',
+    "DELETE FROM training_category WHERE id = ?",
     [req.params.id],
     (err, result) => {
       if (err) {
         console.log(err);
-        res.status(500).send('Error deleting an training category');
+        res.status(500).send("Error deleting an training category");
       } else {
-        if (result.affectedRows) res.status(200).send('🎉 training category deleted!');
-        else res.status(404).send('training category not found.');
+        if (result.affectedRows)
+          res.status(200).send("🎉 training category deleted!");
+        else res.status(404).send("training category not found.");
       }
     }
   );
