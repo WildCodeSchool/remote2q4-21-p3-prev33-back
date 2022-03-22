8b864441e3ac01a5c9762f179479ba3d5fd2f377
sam-rached <rachedsamir@gmail.com>
2022-03-22 09:55:14 +0100

    create carrousel.js modify carrousel.routes.js

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const connection = require('./db-config');
-const express = require('express');
-const cors = require('cors');
+const connection = require("./db-config");
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const router = require('./routes/index.routes');
+const router = require("./routes/index.routes");
 
 const port = process.env.PORT || 8000;
 
 connection.connect((err) => {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
   } else {
-    console.log('connected as id ' + connection.threadId);
+    console.log("connected as id " + connection.threadId);
   }
 });
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
-app.use('/api', router);
+app.use(cors());
+app.use("/api", router);
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
-    res.send("Welcome");
+  res.send("Welcome");
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
