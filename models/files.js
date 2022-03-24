60055f7ea2e9a7ccb37382767f2d274247f8b952
Romain JUGAL <romain-jugal_student2021@wilder.school>
2022-03-24 17:18:05 +0100

    modification of training.routes

---
+++ models/files.js
@@ -0,0 +1,42 @@
+const connection = require("../db-config");
+
+const findAll = () => {
+    return connection
+        .promise()
+        .query('SELECT * FROM training');
+};
+
+const findOne = (id) => {
+    return connection
+        .promise()
+        .query("SELECT * FROM training WHERE id = ?", [id])
+        .then(([results]) => results[0]);
+};
+
+const insertFile = ({
+        reference,
+        title
+    },
+    link) => {
+    return connection
+        .promise()
+        .query("INSERT INTO training (`reference`, `title`, `link`) VALUES (?, ?, ?)", [
+            reference,
+            title,
+            link,
+        ])
+};
+
+const deleteFile = (id) => {
+    return connection
+        .promise()
+        .query("DELETE FROM training WHERE id = ?", [id])
+        .then(([result]) => result.affectedRows !== 0);
+};
+
+module.exports = {
+    findAll,
+    findOne,
+    insertFile,
+    deleteFile,
+};
