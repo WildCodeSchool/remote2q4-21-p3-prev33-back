e1b18aab4cc6e4ad02848700f2c3c38d7b6b4776
sam-rached <rachedsamir@gmail.com>
2022-04-08 14:17:28 +0200

    modify data.sql

--- data.sql
+++ data.sql
@@ -64,7 +64,8 @@ VALUE
 ("Assistance Risques Pro",
 "https://images.unsplash.com/photo-1624638760852-8ede1666ab07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"),
 ("Conseil",
-"https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
+"https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
+("-","");
 
 INSERT INTO `training` (title, training_category_id) 
 VALUE
